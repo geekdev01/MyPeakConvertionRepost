c68290a34d4f2b9b9d9f0a2257acb49eb69ae691
Tresor_geekdev01 <142498098+geekdev01@users.noreply.github.com>
2024-10-04 09:42:45 +0100

    Add files via upload

--- section-story/app.js
+++ section-story/app.js
@@ -1,3 +1,5 @@
+//Affichage du bouton de recherche lorsque l'on survole l'image avec la souris(effet hover)
+//start
 document.getElementById('div-main-image').addEventListener('mouseenter', () => {
     document.querySelector('.btn-main-image').style.display = 'block';
 });
@@ -18,6 +20,10 @@ document.querySelectorAll('.div-detail-image').forEach((elt, index) => {
     });
 });
 
+//start
+
+
+//Affichage des images en grand format lorsque l'on clique dessus 
 document.getElementById('div-main-image').addEventListener('click', () => {
     document.querySelector('.gallerie-image-cover').style.display = 'block';
 });
@@ -32,6 +38,50 @@ document.querySelector('.gallerie-image-cover').addEventListener('click', () => 
 });
 
 
+
+//Affichage de la video (la storie)
+const thumbnails = document.querySelectorAll('.video-thumbnail');
+const modal = document.getElementById('videoModal');
+const video = modal.querySelector('video');
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+        const videoSrc = thumbnail.querySelector('video').src;
+        video.src = videoSrc;
+        modal.style.display = 'block';
+        video.play();
+
+    });
+});
+
+document.querySelector('.tools-btn-out').addEventListener('click', () => {
+    modal.style.display = 'none';
+    video.src = '';
+});
+
+document.querySelector('.tools-btn-pause').addEventListener('click', () => {
+    video.pause();
+    document.querySelector('.tools-btn-pause').style.display = 'none';
+    document.querySelector('.tools-btn-play').style.display = 'block';
+});
+
+document.querySelector('.tools-btn-play').addEventListener('click', () => {
+    video.play();
+    document.querySelector('.tools-btn-pause').style.display = 'block';
+    document.querySelector('.tools-btn-play').style.display = 'none';
+});
+
+document.querySelector('.tools-btn-son').addEventListener('click', () => {
+    video.muted=true;
+    document.querySelector('.tools-btn-son').style.display = 'none';
+    document.querySelector('.tools-btn-muted').style.display = 'block';
+});
+
+document.querySelector('.tools-btn-muted').addEventListener('click', () => {
+    video.muted=false;
+    document.querySelector('.tools-btn-muted').style.display = 'none';
+    document.querySelector('.tools-btn-son').style.display = 'block';
+});
 
 
 
